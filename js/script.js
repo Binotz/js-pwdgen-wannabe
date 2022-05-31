83d27fe73e1756051d356d3f79782c1658a7ff7c
Binotz <binotto.riccardo@gmail.com>
2022-05-31 15:20:04 +0200

    Done

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const lastName = prompt("Ciao, inserisci il tuo cognome: ");
 const color = prompt("Ciao, inserisci il tuo colore preferito: ");
 
 //Password generation
-let password = firstName + lastName + color +'21';
+let password = 'Ecco la tua nuova password super segreta: ' + firstName + lastName + color +'21';
 
 //Data output
 document.getElementById('pass').innerHTML = password;
